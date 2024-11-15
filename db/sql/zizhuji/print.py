09852d5dd90370b09d850e3163d02fae13fe58a7
yunZ <openhisauthor@outlook.com>
2024-11-15 22:34:37 +0800

    添加假脚本

---
+++ db/sql/zizhuji/print.py
@@ -0,0 +1,46 @@
+printInfoHeaderSQL = '''
+select 1
+where 1<>?
+'''
+zongFeiYongSQL = '''
+select 1
+where 1<>?
+'''
+zhifuFangshiSQL = '''
+select 1
+where 1<>?
+'''
+fapiaoURLSQL = '''
+select 1
+where 1<>?
+'''
+feibieSQL = '''
+select 1
+where 1<>?
+''' 
+zhiyindanSQL = '''
+select 1
+where 1<>?
+and 1<>?
+'''
+yingxiangdanSQL = '''
+select 1
+where 1<>?
+and 1<>?
+'''
+zhuyaozhenduanSQL = '''
+select 1
+where 1<>?
+'''
+weicaidanSQL = '''
+select 1
+where 1<>?
+'''
+caixiedanSQL = '''
+select 1
+where 1<>?
+'''
+fukedanSQL = '''
+select 1
+where 1<>?
+'''

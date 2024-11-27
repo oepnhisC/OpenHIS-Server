8c55455a30fbe73761fe5f18c5393d880a5b4541
yunZ <openhisauthor@outlook.com>
2024-11-27 21:39:58 +0800

    添加结账管理查询功能

--- api/shoufei/urls.py
+++ api/shoufei/urls.py
@@ -1,28 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Request
 from db.database import * 
-from db.sql.shoufei.paidList import paidListSQL
+from pydantic import BaseModel
+from db.sql.shoufei.jiezhangdanjuSQL import jiezhangdanjuSQL
 
 shoufeiAPI = APIRouter(prefix="/shoufei",tags=["收费"])
 
-@shoufeiAPI.get("/")
-async def index():
-    return {"message": "收费"}
 
+class ShiJian(BaseModel):
+    begintime: str
+    endtime: str
 
+@shoufeiAPI.post("/jiezhangdanju")
+async def jiezhangdanju(request: Request,shijian: ShiJian):
 
+    print(shijian.dict())
+    if not shijian.begintime or not shijian.endtime:
+        responJson = {'code':1,'result':'时间不能为空'}
+        return  responJson
 
-
-@shoufeiAPI.get("/paidList")
-async def paidList():
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute(paidListSQL)
+    cursor.execute(jiezhangdanjuSQL,(shijian.begintime,shijian.endtime
+                                     ,shijian.begintime,shijian.endtime
+                                     ,shijian.begintime,shijian.endtime
+                                     ,shijian.begintime,shijian.endtime))
     rows = cursor.fetchall()
-    # 获取列名
     columns = [column[0] for column in cursor.description]
-    # 将每一行转换为字典
-    data = [dict(zip(columns, row)) for row in rows]
- 
     cursor.close()
     conn.close()
-    return {"data": data}
+    if len(rows) == 0:
+        responJson = {'code':1,'result':'未查询到相关信息'}
+        return  responJson
+    else:
+        data = [dict(zip(columns, row)) for row in rows]
+        responJson = { 'code':0,'result':data }
+    
+    return  responJson

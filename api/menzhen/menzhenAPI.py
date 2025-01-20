f93f8bb193a13b3c896d104633a687d63fe4b126
yunZ <openhisauthor@outlook.com>
2025-01-20 22:45:34 +0800

    添加搜索病人功能

--- api/menzhen/menzhenAPI.py
+++ api/menzhen/menzhenAPI.py
@@ -8,6 +8,9 @@ menzhenAPI = APIRouter(prefix="/menzhen",tags=["门诊"])
 class JiuZhenXinXi(BaseModel):
     jzid:str
 
+
+
+
 @menzhenAPI.post('/getHouZhenList')
 async def getHouZhenList(request:Request):
     '''
@@ -51,6 +54,39 @@ async def getYiZhuList(request:Request,jiuZhenXinXi:JiuZhenXinXi):
         return {'code':2,'result':'无就诊ID'}
     responJson = {}
     rows, columns = execute_query(getYiZhuListSQL,(jiuZhenXinXi.jzid,))
+    if len(rows) == 0:
+        responJson = {'code':1,'result':'未查询到相关信息'}
+        return  responJson
+    else:
+        data = [dict(zip(columns, row)) for row in rows]
+        responJson = { 'code':0,'result':data }
+
+    return responJson
+
+class ChaXunBingRen(BaseModel):
+    beginTime:str
+    endTime:str
+    content:str
+    mzh:str
+
+
+@menzhenAPI.post('/chaXunBingRen')
+def chaXunBingRen(request:Request,chaxunBingRen:ChaXunBingRen):
+    '''
+    搜索病人信息
+    '''
+    responJson = {}
+    if not chaxunBingRen.beginTime or not chaxunBingRen.endTime :
+        return {'code':2,'result':'必须选择时间范围'}
+    if not chaxunBingRen.content:
+        return {'code':2,'result':'必须输入搜索内容'}
+    rows, columns = execute_query(chaXunOneBingRenSQL
+                        ,(chaxunBingRen.beginTime
+                          ,chaxunBingRen.endTime
+                          ,chaxunBingRen.content
+                          ,chaxunBingRen.content
+                          ,chaxunBingRen.mzh
+                          ,chaxunBingRen.content))
     if len(rows) == 0:
         responJson = {'code':1,'result':'未查询到相关信息'}
         return  responJson
